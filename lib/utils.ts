e28f1fbf8b7004c1b9a89e5967d5fb8651313873
Alex Straight <compsciaj@outlook.com>
2024-03-02 20:43:35 -0800

    ........

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,23 @@ import { clsx, type ClassValue } from "clsx";
 import { UserIcon } from "lucide-react";
 import React from "react";
 import { twMerge } from "tailwind-merge";
-import { icons } from "./data";
+
+import {
+  FaFacebookF,
+  FaGithub,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+} from "react-icons/fa";
+import { Icon } from "./types";
+
+export const icons: { [key: string]: Icon } = {
+  linkedin: { icon: React.createElement(FaLinkedinIn), name: "LinkedIn" },
+  twitter: { icon: React.createElement(FaTwitter), name: "Twitter" },
+  github: { icon: React.createElement(FaGithub), name: "GitHub" },
+  facebook: { icon: React.createElement(FaFacebookF), name: "Facebook" },
+  instagram: { icon: React.createElement(FaInstagram), name: "Instagram" },
+};
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
